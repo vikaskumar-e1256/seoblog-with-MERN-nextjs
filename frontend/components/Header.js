fe9cf8ac327f8c63aacbd50e2c003870f1a87a9d
Nishant <vikaskumar.e1256@gmail.com>
2024-01-17 16:41:58 +0530

    nprogress package added and implemented

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -12,6 +12,11 @@ import { APP_NAME } from '../config';
 import Link from 'next/link';
 import { isAuth, signout } from '../actions/auth';
 import Router from 'next/router';
+import NProgress from 'nprogress';
+
+Router.onRouteChangeStart = url => NProgress.start();
+Router.onRouteChangeComplete = url => NProgress.done();
+Router.onRouteChangeError = url => NProgress.done();
 
 function Header(args) {
     const [isOpen, setIsOpen] = useState(false);
