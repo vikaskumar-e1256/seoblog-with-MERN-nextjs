cbec9273c0e25f23719522fb953e5f2c6e6ce543
Nishant <vikaskumar.e1256@gmail.com>
2024-01-21 11:30:01 +0530

    protect unwanted error showing if token expired

--- frontend/components/auth/Admin.js
+++ frontend/components/auth/Admin.js
@@ -1,9 +1,31 @@
 import React, { useEffect } from 'react';
 import { isAuth } from '../../actions/auth';
 import Router from 'next/router';
+import { API } from "../../config";
+import { getCookie } from '../../actions/auth';
+
 
 function Admin({ children }) {
+    const token = getCookie('token');
     useEffect(() => {
+        const checkAuth = async () => {
+            try {
+                const response = await fetch(`${API}/api/profile`,
+                    {
+                        method: "GET",
+                        headers: {
+                            Accept: "application/json",
+                            "Content-type": "application/json",
+                            Authorization: `Bearer ${token}`
+                        },
+                    });
+                if (response.status === 401) {
+                    Router.push('/signin');
+                }
+            } catch (error) {
+                console.error('Error checking authentication:', error);
+            }
+        };
         if (!isAuth()) {
             Router.push('/signin');
         }else if (isAuth().role !== 1) {
