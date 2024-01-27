d8d9d29c811f2d1bec8e68127100720b9d61a0bb
Nishant <vikaskumar.e1256@gmail.com>
2024-01-27 16:23:33 +0530

    get only single image from any blog by slug

--- backend/routes/blogRoutes.js
+++ backend/routes/blogRoutes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { create, list, read, listAllBlogsCategoriesTags, remove, update } = require("../controllers/blogController");
+const { create, list, read, listAllBlogsCategoriesTags, remove, update, photo } = require("../controllers/blogController");
 const router = express.Router();
 const { requireSignin, adminMiddleware } = require("../controllers/authController");
 
@@ -10,5 +10,6 @@ router.post("/blogs-categories-tags", listAllBlogsCategoriesTags);
 router.get("/blog/:slug", read);
 router.delete("/blog/:slug", requireSignin, adminMiddleware, remove);
 router.put("/blog/:slug", requireSignin, adminMiddleware, update);
+router.get("/blog/photo/:slug", photo);
 
 module.exports = router;
